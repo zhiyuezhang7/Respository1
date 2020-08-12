ca27400f90d754ef2bcaf357ca034419cc31a32d
Zhiyue Zhang <joyuzaizhang@Outlook.com>
2020-08-12 22:17:22 +0800

    2

---
+++ JavaSyntax.java
@@ -0,0 +1,44 @@
+//  i   want to know if this is working
+/**
+ * trial1
+ */
+public class JavaSyntax {
+
+  public static void main(String[] args) {
+    System.out.println("Test");
+
+    // type variableName = value;
+    String name = "John";
+    System.out.println(name);
+    boolean name1 = true;
+    System.out.println(name1);
+    int name2 = 5555;
+    System.out.println(name2);
+    char name3 = 'c';
+    System.out.println(name3);
+    String name4 = "Thank you my computer";
+    System.out.println(name4);
+    float myFloatNum = 5.99f;
+    System.out.println(myFloatNum);
+
+    final int myNum = 15;
+    // myNum = 20;
+    // will generate an error: cannot assign a value to a final variable
+    System.out.println(myNum);
+
+    int x = 5, y = 6, z = 50;
+    System.out.println(x + y + z);
+
+    /*
+     * byte 1 byte Stores whole numbers from -128 to 127 short 2 bytes Stores whole
+     * numbers from -32,768 to 32,767 int 4 bytes Stores whole numbers from
+     * -2,147,483,648 to 2,147,483,647 long 8 bytes Stores whole numbers from
+     * -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 float 4 bytes Stores
+     * fractional numbers. Sufficient for storing 6 to 7 decimal digits double 8
+     * bytes Stores fractional numbers. Sufficient for storing 15 decimal digits
+     * boolean 1 bit Stores true or false values char 2 bytes Stores a single
+     * character/letter or ASCII values
+     */
+
+  }
+}
