603d7ed62d25cc2cb319a116b688659c3ad290cf
Zhiyue Zhang <joyuzaizhang@Outlook.com>
2020-08-18 00:47:30 +0800

    1

---
+++ lecture0/trial.java
@@ -0,0 +1,10 @@
+//  i   want to know if this is working
+/**
+ * trial1
+ */
+public class trial {
+
+   public static void main(String[] args) {
+     System.out.println("Test");
+   }
+}
---
+++ lecture0/trial1.java
@@ -0,0 +1,10 @@
+//  i   want to know if this is working
+/**
+ * trial1
+ */
+public class trial1 {
+
+   public static void main(String[] args) {
+     System.out.println("Test");
+   }
+}
